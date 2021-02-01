cc14bb18a315bf95f7b15fd7227c3e74d7fd6647
russ-vy <russ.letter@gmail.com>
2021-02-02 00:42:38 +0300

    lesson5

---
+++ lesson5/lesson/view/gallery.php
@@ -0,0 +1,18 @@
+<div class="container">
+
+    <form class="form-file" enctype="multipart/form-data" method="POST">
+        <input type="file" name="photo" accept="image/*,image/jpeg" class="form-file-input" id="customFile">
+        <label class="form-file-label" for="customFile">
+            <span class="form-file-text">Choose file...</span>
+            <span class="form-file-button">Browse</span>
+        </label>
+        <button class="form-file-button send" type="submit" data-onclick="fileUpload()">Send</button>
+    </form>
+
+    <div class="gallery row">
+
+        <?php   getImages();    ?>
+
+    </div>
+
+</div>
